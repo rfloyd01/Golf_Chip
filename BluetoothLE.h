0e7e5af91fc5bf8b0eb10f6169479cdc9b4e36e2
rfloyd01 <70585609+rfloyd01@users.noreply.github.com>
2020-09-14 17:55:34 -0400

    Update BluetoothLE.h

--- BluetoothLE.h
+++ BluetoothLE.h
@@ -19,12 +19,17 @@ public:
 
 private:
 	float ConvertInt32toFloat(int32_t num);
+	float accl_conversion = 9.81 * 4.0 / 32768.0;
+	float gyr_conversion = 2000.0 / 32768.0;
+	float mag_conversion = 4.0 * 100.0 / 32768.0;
 
 	void SetUpDeviceWatcher();
 	concurrency::task<void> connectToBLEDevice(unsigned long long bluetoothAddress);
 
 	guid service_UUID, characteristic_UUID;
 	unsigned long long address;
+	float time_average = 0;
+	int cycle_count = 0;
 
 	std::wstring formatBluetoothAddress(unsigned long long BluetoothAddress);
 	std::chrono::steady_clock::time_point timer;
