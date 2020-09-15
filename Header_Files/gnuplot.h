13d8896e906ca69c2cc39d9ae895f3eb681511bc
rfloyd01 <70585609+rfloyd01@users.noreply.github.com>
2020-09-15 15:32:00 -0400

    Create gnuplot.h

---
+++ Header_Files/gnuplot.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class Gnuplot
+{
+public:
+    Gnuplot();
+    ~Gnuplot();
+    void operator ()(const string& command);
+    // send any command to gnuplot
+protected:
+    FILE* gnuplotpipe;
+};
