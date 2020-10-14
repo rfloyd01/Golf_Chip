a6995d4a4a5ca210e3e3b572b793ba144fc91e9b
rfloyd01 <70585609+rfloyd01@users.noreply.github.com>
2020-10-13 20:21:05 -0400

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -12,3 +12,10 @@ void print(glm::quat q)
 {
 	std::cout << '{' << q.w << ", " << q.x << ", " << q.y << ", " << q.z << '}' << std::endl;
 }
+
+void vprint(std::vector<float> vec)
+{
+	std::cout << '[';
+	for (int i = 0; i < vec.size() - 1; i++) std::cout << vec[i] << ", ";
+	std::cout << vec.back() << ']' << std::endl;
+}
