4d92fe9a0a8fb6a850bde8a86798a22d5228e629
rfloyd01 <70585609+rfloyd01@users.noreply.github.com>
2020-10-23 20:40:47 -0400

    Update sensor_fusion.h

--- Header_Files/sensor_fusion.h
+++ Header_Files/sensor_fusion.h
@@ -4,7 +4,7 @@
 #include <Header_Files/glm.h>
 
 glm::quat Madgwick(glm::quat q, float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz, float delta_t, float beta);
-glm::quat MadgwickModified(glm::quat q, float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz, float delta_t, float beta);
+glm::quat MadgwickModified(glm::quat q, float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz, glm::quat h, float delta_t, float beta);
 glm::quat MadgwickIMU(glm::quat q, float gx, float gy, float gz, float ax, float ay, float az, float delta_t, float beta);
 glm::quat Floyd(glm::quat q, float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz, float bx, float by, float bz, float delta_t, float beta); //attempt at making my own algorithm
 
