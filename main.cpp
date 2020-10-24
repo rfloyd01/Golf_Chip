e58314fcf377361733d66afc74a78f9badbb0620
rfloyd01 <70585609+rfloyd01@users.noreply.github.com>
2020-10-23 20:53:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <Header_Files/BluetoothLE.h>
 #include <Header_Files/graphics.h>
+#include <Header_Files/model.h>
 
 using namespace winrt;
 using namespace Windows::Foundation;
@@ -20,12 +21,12 @@ int main()
     //Create BLE device and wait for it to connect to actual BLE device
     float sensor_refresh_rate = 400; //chip is set to gather all forms of data at 400Hz
     BLEDevice BLE_Nano(serviceUUID, sensor_refresh_rate);
-    BLE_Nano.Connect();
+    BLE_Nano.connect();
     while (BLE_Nano.data_available == false) //wait until chip has connected and started to gather data before moving onto the next step
     {
     }
 
-    BLE_Nano.SetMagField();
+    BLE_Nano.setMagField();
 
     //Set up calibration
     Calibration Cal(&BLE_Nano);
@@ -33,33 +34,24 @@ int main()
     //Set up OpenGL and Shaders
     GL GraphicWindow(&BLE_Nano);
     GraphicWindow.LoadTexture("FXOS8700.jpg");
-    GraphicWindow.AddText({ "Press Space to enter calibration mode", 520.0f, 10.0f, 0.33f, glm::vec3(1.0f, 1.0f, 1.0f) });
+    GraphicWindow.AddText(MessageType::FOOT_NOTE, { "Press Space to enter calibration mode.", 520.0f, 10.0f, 0.33f, glm::vec3(1.0f, 1.0f, 1.0f), GraphicWindow.getScreenWidth() });
 
     //Connect Calibration class to graphic interface
     Cal.SetGraphics(&GraphicWindow);
     GraphicWindow.setCal(&Cal);
-    BLE_Nano.SetPositionTimer();
 
-    //Disable V-Sync so that frames aren't limited by display
-    glfwSwapInterval(0);
-    double loop_timer = glfwGetTime();
+    //glfwSwapInterval(0); //If this is uncommented it will disable v-sync, allowing OpenGl to update faster than the monitor refresh rate (60 Hz in this case)
 
-    //for (int i = 1; i < test_times.size(); i++)
+    //Main rendering loop
     while (!GraphicWindow.ShouldClose())
     {
+        //All updates for sensor data are taking place behing the scenes, values are updated when new data comes in from a concurrent function
+        //That's why (for now) the only thing in this loop has to do with the graphic window
         GraphicWindow.Update(); //Check for input, check for render data, etc.
-        GraphicWindow.Render(); //Set matrices, render image, render text, etc.
-
-        BLE_Nano.Update(); //Read any new sensor data and update current chip info
-
-        //Add in a hard wait time before next loop iteration that is equal to 2/3 of the connection interval with the BLE chip divided by the number of samples
-        while ((glfwGetTime() - loop_timer) * 1000 < 2.4) //2.4 milliseconds has been calculated by hand here, but put in a hard calculation in the code at some point
-        {
-        }
-        loop_timer = glfwGetTime();
+        GraphicWindow.SetClubMatrices({ 1.0, 1.0, 1.0 }, BLE_Nano.getLocation()); //set scale and translation matrices for render, rotation matrix is handled by Madgwick filter separately
+        GraphicWindow.Render(BLE_Nano.getOpenGLQuaternion()); //Set matrices, render image, render text, etc.
     }
 
-    GraphicWindow.DeleteBuffers();
     GraphicWindow.Terminate();
 
     return 0;
